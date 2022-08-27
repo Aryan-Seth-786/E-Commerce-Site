1b6fd4fab77e00998c1bcad71ebb8632dda3b944
Aryan_seth <aryanseth81@gmail.com>
2022-08-28 04:37:45 +0530

    added not found page

--- resources/js/src/pages/index.vue
+++ resources/js/src/pages/index.vue
@@ -3,3 +3,15 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 router.push("/Home");
 </script> -->
+<template >
+    <div id="notFound">
+        <p>404 not found</p>
+        <a href="/">goto home page</a>
+    </div>
+</template>
+<script>
+
+</script>
+<style lang="css">
+    
+</style>
--- resources/js/src/routes.js
+++ resources/js/src/routes.js
@@ -2,6 +2,7 @@ import Home from "./pages/Home/index.vue";
 
 const routes = [
     {path: '/', name: 'home', component: Home},
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: ()=>import('./pages/index.vue')},
 ];
 
 export default routes;
